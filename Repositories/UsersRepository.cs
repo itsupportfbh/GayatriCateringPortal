5fcbe23bf2a598f156147a722ee3973a16a29823
antonyvinoth3725 <antonyvinoth3725@gmail.com>
2026-07-13 07:42:20 +0530

    Changes

---
+++ Repositories/UsersRepository.cs
@@ -0,0 +1,325 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using GayatriCateringPortal.Data;
+using GayatriCateringPortal.Interfaces;
+using GayatriCateringPortal.Models;
+
+namespace GayatriCateringPortal.Repositories;
+
+public class UsersRepository : IUsersRepository
+{
+    public List<UserMaster> GetAll()
+    {
+        List<UserMaster> list = new List<UserMaster>();
+        IDbConnection conn = null;
+        IDbCommand cmd = null;
+        IDataReader reader = null;
+
+        try
+        {
+            using (conn = DataFactory.CreateConnection())
+            {
+                conn.Open();
+                using (cmd = DataFactory.CreateCommand("GetUserMaster", conn))
+                {
+                    ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
+                    reader = DataFactory.ExecuteReader(cmd);
+                    list = this.List(reader);
+                }
+            }
+
+            return list ?? new List<UserMaster>();
+        }
+        catch (SqlException)
+        {
+            throw new Exception("Database error");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.StackTrace);
+        }
+        finally
+        {
+            if (conn != null && conn.State != ConnectionState.Closed) conn.Close();
+        }
+    }
+
+    public UserMaster? GetById(int id)
+    {
+        IDbConnection conn = null;
+        IDbCommand cmd = null;
+        IDataReader reader = null;
+        UserMaster? item = null;
+
+        try
+        {
+            using (conn = DataFactory.CreateConnection())
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                using (cmd = DataFactory.CreateCommand("SP_GetUserMasterByID", conn))
+                {
+                    ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Id", id));
+                    reader = DataFactory.ExecuteReader(cmd);
+                    var list = this.List(reader);
+                    if (list != null && list.Count > 0) item = list[0];
+                }
+            }
+        }
+        catch (SqlException)
+        {
+            throw new Exception("Database error");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.StackTrace);
+        }
+        finally
+        {
+            if (conn != null && conn.State != ConnectionState.Closed) conn.Close();
+        }
+
+        return item;
+    }
+
+    public int Create(UserMaster item)
+    {
+        IDbConnection conn = null;
+        IDbCommand cmd = null;
+        try
+        {
+            using (conn = DataFactory.CreateConnection())
+            {
+                conn.Open();
+                using (cmd = DataFactory.CreateCommand("SP_CreateUserMaster", conn))
+                {
+                    ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Code", item.Code));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Name", item.Name));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Email", item.Email));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Password", item.Password));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@ContactNo", item.ContactNo ?? (object)DBNull.Value));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Remarks", item.Remarks ?? (object)DBNull.Value));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@IsAdmin", item.IsAdmin));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@IsActive", item.IsActive));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@IsDeleted", item.IsDeleted));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@CreatedBy", item.CreatedBy ?? (object)DBNull.Value));
+
+                    var result = DataFactory.ExecuteScalar(cmd);
+                    if (result != null)
+                    {
+                        return Convert.ToInt32(result);  // Returns -1 if duplicate, > 0 if success, 0 if error
+                    }
+                }
+            }
+        }
+        catch (SqlException)
+        {
+            return 0;  // Return 0 on database error
+        }
+        catch (Exception ex)
+        {
+            return 0;  // Return 0 on any other error
+        }
+        finally
+        {
+            if (conn != null && conn.State != ConnectionState.Closed) conn.Close();
+        }
+
+        return 0;
+    }
+
+    public int Update(UserMaster item)
+    {
+        IDbConnection conn = null;
+        IDbCommand cmd = null;
+        try
+        {
+            using (conn = DataFactory.CreateConnection())
+            {
+                conn.Open();
+                using (cmd = DataFactory.CreateCommand("SP_UpdateUserMaster", conn))
+                {
+                    ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Id", item.Id));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Code", item.Code));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Name", item.Name));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Email", item.Email));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Password", item.Password));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@ContactNo", item.ContactNo ?? (object)DBNull.Value));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Remarks", item.Remarks ?? (object)DBNull.Value));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@IsAdmin", item.IsAdmin));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@IsActive", item.IsActive));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@IsDeleted", item.IsDeleted));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@UpdatedBy", item.UpdatedBy ?? (object)DBNull.Value));
+
+                    var result = DataFactory.ExecuteScalar(cmd);
+                    if (result != null)
+                    {
+                        int status = Convert.ToInt32(result);
+                        return status;  // Returns -1 if duplicate, > 0 if success, 0 if error
+                    }
+                }
+            }
+        }
+        catch (SqlException)
+        {
+            return 0;  // Return 0 on database error
+        }
+        catch (Exception ex)
+        {
+            return 0;  // Return 0 on any other error
+        }
+        finally
+        {
+            if (conn != null && conn.State != ConnectionState.Closed) conn.Close();
+        }
+        return 0;
+    }
+
+    public bool Delete(int id)
+    {
+        IDbConnection conn = null;
+        IDbCommand cmd = null;
+        bool taskStatus = false;
+
+        try
+        {
+            using (conn = DataFactory.CreateConnection())
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                using (cmd = DataFactory.CreateCommand("DeleteUserMasterById", conn))
+                {
+                    ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Id", id));
+                    var r = DataFactory.ExecuteScalar(cmd);
+                    if (r != null && Convert.ToInt32(r) > 0)
+                        taskStatus = true;
+                }
+            }
+        }
+        catch (SqlException)
+        {
+            throw new Exception("Database error");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.StackTrace);
+        }
+        finally
+        {
+            if (conn != null && conn.State != ConnectionState.Closed) conn.Close();
+        }
+
+        return taskStatus;
+    }
+
+    public bool ActiveInActive(int id, bool status)
+    {
+        IDbConnection conn = null;
+        IDbCommand cmd = null;
+        try
+        {
+            using (conn = DataFactory.CreateConnection())
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                using (cmd = DataFactory.CreateCommand("ActiveInActiveUserMasterById", conn))
+                {
+                    ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@Id", id));
+                    cmd.Parameters.Add(DataFactory.CreateParameter("@IsActive", status));
+                    var r = DataFactory.ExecuteScalar(cmd);
+                    return r != null && Convert.ToInt32(r) > 0;
+                }
+            }
+        }
+        catch (SqlException)
+        {
+            throw new Exception("Database error");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.StackTrace);
+        }
+        finally
+        {
+            if (conn != null && conn.State != ConnectionState.Closed) conn.Close();
+        }
+    }
+
+    #region Private Methods
+    private List<UserMaster> List(IDataReader reader)
+    {
+        var list = new List<UserMaster>();
+        try
+        {
+            while (reader.Read())
+            {
+                var item = new UserMaster();
+                if (reader["Id"] != DBNull.Value)
+                    item.Id = Convert.ToInt32(reader["Id"])!;
+                if (reader["Code"] != DBNull.Value)
+                    item.Code = Convert.ToString(reader["Code"])!;
+                if (reader["Name"] != DBNull.Value)
+                    item.Name = Convert.ToString(reader["Name"])!;
+                if (reader["Email"] != DBNull.Value)
+                    item.Email = Convert.ToString(reader["Email"])!;
+                if (reader["Password"] != DBNull.Value)
+                    item.Password = Convert.ToString(reader["Password"])!;
+                if (reader["ContactNo"] != DBNull.Value)
+                    item.ContactNo = Convert.ToString(reader["ContactNo"]);
+                if (reader["Remarks"] != DBNull.Value)
+                    item.Remarks = Convert.ToString(reader["Remarks"]);
+                if (reader["IsAdmin"] != DBNull.Value)
+                    item.IsAdmin = Convert.ToBoolean(reader["IsAdmin"]);
+                if (reader["IsActive"] != DBNull.Value)
+                    item.IsActive = Convert.ToBoolean(reader["IsActive"]);
+                if (reader["IsDeleted"] != DBNull.Value)
+                    item.IsDeleted = Convert.ToBoolean(reader["IsDeleted"]);
+                if (reader["CreatedBy"] != DBNull.Value)
+                    item.CreatedBy = Convert.ToInt32(reader["CreatedBy"]);
+                if (reader["CreatedDate"] != DBNull.Value)
+                    item.CreatedDate = Convert.ToDateTime(reader["CreatedDate"]);
+                if (reader["UpdatedBy"] != DBNull.Value)
+                    item.UpdatedBy = Convert.ToInt32(reader["UpdatedBy"]);
+                if (reader["UpdatedDate"] != DBNull.Value)
+                    item.UpdatedDate = Convert.ToDateTime(reader["UpdatedDate"]);
+                if (reader["Image"] != DBNull.Value)
+                    item.Image = Convert.ToString(reader["Image"]);
+                if (reader["Gender"] != DBNull.Value)
+                    item.Gender = Convert.ToInt32(reader["Gender"]);
+                if (reader["DOB"] != DBNull.Value)
+                    item.DOB = Convert.ToDateTime(reader["DOB"]);
+                if (reader["Age"] != DBNull.Value)
+                    item.Age = Convert.ToInt32(reader["Age"]);
+                if (reader["Address1"] != DBNull.Value)
+                    item.Address1 = Convert.ToString(reader["Address1"]);
+                if (reader["Address2"] != DBNull.Value)
+                    item.Address2 = Convert.ToString(reader["Address2"]);
+                if (reader["Country"] != DBNull.Value)
+                    item.Country = Convert.ToInt32(reader["Country"]);
+                if (reader["State"] != DBNull.Value)
+                    item.State = Convert.ToInt32(reader["State"]);
+                if (reader["City"] != DBNull.Value)
+                    item.City = Convert.ToInt32(reader["City"]);
+                if (reader["PostalCode"] != DBNull.Value)
+                    item.PostalCode = Convert.ToString(reader["PostalCode"]);
+
+                list.Add(item);
+            }
+        }
+        catch (SqlException)
+        {
+            throw new Exception("Database error");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.StackTrace);
+        }
+
+        return list;
+    }
+    #endregion
+}
---
+++ wwwroot/js/admin/foodCategory.js
@@ -0,0 +1,293 @@
+$(document).ready(function () {
+    loadFoodCategories();
+});
+
+function closeFoodCategoryModal() {
+    $('#foodCategoryModal').addClass('hidden');
+}
+
+function openFoodCategoryModal() {
+    clearFoodCategoryForm();
+    $('#modal-title').html('Create Food Category');
+    $('#foodCategoryModal').removeClass('hidden');
+
+    initCategoryField('#categoryCode', '#categoryCodeError');
+    initCategoryField('#categoryName', '#categoryNameError');
+    
+    $('#saveCategoryBtn').text('Save');
+}
+
+function loadFoodCategories() {
+    showCategoryLoader(true);
+
+    $.ajax({
+        url: '/Admin/FoodMenuCategories/get',
+        type: 'GET',
+        dataType: 'json',
+        success: function (rows) {
+            renderFoodCategoriesList(Array.isArray(rows) ? rows : []);
+        },
+        error: function () {
+            renderFoodCategoriesList([]);
+            showToast('Unable to load food categories.', 3000, { type: 'error', title: 'Load failed' });
+        },
+        complete: function () {
+            showCategoryLoader(false);
+        }
+    });
+}
+
+function showCategoryLoader(show) {
+    var $panel = $('.pageloaderpanel');
+    if ($panel.length) {
+        $('#foodCategoryListPanel .table-wrap').toggleClass('hidden', show);
+        $panel.toggleClass('hidden', !show);
+        return;
+    }
+}
+
+function renderFoodCategoriesList(rows) {
+    rows = Array.isArray(rows) ? rows : [];
+    var html = '';
+
+    if (rows.length) {
+        for (var i = 0; i < rows.length; i++) {
+            var category = rows[i];
+            var serial = i + 1;
+            var id = category.id || 0;
+            var code = category.code || '';
+            var name = category.name || '';
+            var isActive = category.isActive;
+
+            var actions;
+            if (isActive) {
+                actions = `<button type="button" class="action-item btn-edit" data-id="${id}" onclick="editFoodCategory(this.dataset.id)"><span class="action-icon p-p-pencil"></span>Edit</button>
+                           <button type="button" class="action-item btn-set-inactive" data-id="${id}" onclick="setFoodCategoryActive(this.dataset.id, false)"><span class="action-icon p-p-lock"></span>Inactive</button>`;
+            } else {
+                actions = `<button type="button" class="action-item btn-set-active" data-id="${id}" onclick="setFoodCategoryActive(this.dataset.id, true)"><span class="action-icon p-p-unlock"></span>Active</button>`;
+            }
+
+            var statusBadge;
+            if (isActive) {
+                statusBadge = '<span class="badge-pill badge-pill--success">Active</span>';
+            } else {
+                statusBadge = '<span class="badge-pill badge-pill--warning">Inactive</span>';
+            }
+
+            html += `
+                <tr>
+                    <td>${serial}</td>
+                    <td>${code}</td>
+                    <td>${name}</td>
+                    <td>${statusBadge}</td>
+                    <td>
+                        <div class="row-actions">
+                            <button class="dots-btn" title="Actions">⋯</button>
+                            <div class="actions-menu hidden">
+                                ${actions}
+                                <button type="button" class="action-item btn-delete" data-id="${id}" onclick="deleteFoodCategory(this.dataset.id)"><span class="action-icon p-p-trash"></span>Delete</button>
+                            </div>
+                        </div>
+                    </td>
+                </tr>`;
+        }
+    }
+
+    $('#foodCategoryList tbody').html(html);
+
+    if (typeof renderDataTable === 'function') {
+        renderDataTable('foodCategoryList');
+    }
+}
+
+function setCategoryError(inputSelector, errorSelector, message) {
+    $(inputSelector).addClass('input-error');
+    $(errorSelector).removeClass('hidden').text(message);
+}
+
+function clearCategoryError(inputSelector, errorSelector) {
+    $(inputSelector).removeClass('input-error');
+    $(errorSelector).addClass('hidden').text('');
+}
+
+function initCategoryField(inputSelector, errorSelector) {
+    clearCategoryError(inputSelector, errorSelector);
+    var el = document.querySelector(inputSelector);
+    if (el) {
+        el.oninput = function () {
+            clearCategoryError(inputSelector, errorSelector);
+        };
+    }
+}
+
+function clearFoodCategoryForm(keepId) {
+    if (!keepId) {
+        $('#categoryId').val('');
+        $('#saveCategoryBtn').text('Save');
+    }
+    $('#categoryCode').val('');
+    $('#categoryName').val('');
+    clearCategoryError('#categoryCode', '#categoryCodeError');
+    clearCategoryError('#categoryName', '#categoryNameError');
+}
+
+function editFoodCategory(id) {
+    $('#modal-title').html('Edit Food Category');
+
+    $.ajax({
+        url: '/Admin/FoodMenuCategories/get/' + id,
+        type: 'GET',
+        success: function (category) {
+            if (!category) {
+                showToast('Food category not found.', 3000, { type: 'warning', title: 'Not found' });
+                return;
+            }
+            $('#categoryId').val(category.Id || category.id || '');
+            $('#categoryCode').val(category.Code || category.code || '');
+            $('#categoryName').val(category.Name || category.name || '');
+            
+            initCategoryField('#categoryCode', '#categoryCodeError');
+            initCategoryField('#categoryName', '#categoryNameError');
+
+            $('#foodCategoryModal').removeClass('hidden');
+            $('#saveCategoryBtn').text('Update');
+        },
+        error: function () {
+            showToast('Unable to load food category.', 3000, { type: 'error', title: 'Load failed' });
+        }
+    });
+}
+
+function validateForm() {
+    clearCategoryError('#categoryCode', '#categoryCodeError');
+    clearCategoryError('#categoryName', '#categoryNameError');
+
+    var code = $('#categoryCode').val();
+    if (code) {
+        code = code.toString().trim();
+    } else {
+        code = '';
+    }
+
+    var name = $('#categoryName').val();
+    if (name) {
+        name = name.toString().trim();
+    } else {
+        name = '';
+    }
+
+    var firstInvalid = null;
+
+    if (!code) {
+        setCategoryError('#categoryCode', '#categoryCodeError', 'Code is required');
+        firstInvalid = '#categoryCode';
+    }
+
+    if (!name) {
+        setCategoryError('#categoryName', '#categoryNameError', 'Name is required');
+        if (!firstInvalid) {
+            firstInvalid = '#categoryName';
+        }
+    }
+
+    if (firstInvalid) {
+        $(firstInvalid).focus();
+        return false;
+    }
+
+    return true;
+}
+
+function saveFoodCategory() {
+    if (!validateForm()) {
+        return;
+    }
+
+    var categoryId = $('#categoryId').val();
+    var category = {
+        Id: categoryId ? parseInt(categoryId) : 0,
+        Code: $('#categoryCode').val().trim(),
+        Name: $('#categoryName').val().trim(),
+        IsActive: true,
+        IsDeleted: false,
+        CreatedBy: 0,
+        UpdatedBy: 0
+    };
+
+    var endpoint = category.Id ? '/Admin/FoodMenuCategories/update' : '/Admin/FoodMenuCategories/create';
+    var isUpdate = category.Id > 0;
+
+    $.ajax({
+        url: endpoint,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(category),
+        success: function (res) {
+            if (res && res.success) {
+                showToast(isUpdate ? 'Food category updated successfully.' : 'Food category created successfully.', 3000, { type: 'success', title: isUpdate ? 'Updated' : 'Created' });
+                clearFoodCategoryForm();
+                $('#foodCategoryModal').addClass('hidden');
+                loadFoodCategories();
+            } else {
+                var errorMsg = res && res.message ? res.message : (isUpdate ? 'Unable to update food category.' : 'Unable to create food category.');
+                showToast(errorMsg, 3000, { type: 'error', title: 'Save failed' });
+            }
+        },
+        error: function () {
+            showToast(isUpdate ? 'Update failed.' : 'Save failed.', 3000, { type: 'error', title: 'Save failed' });
+        }
+    });
+}
+
+function deleteFoodCategory(id) {
+    if (!id) return;
+
+    var confirmMsg = 'Are you sure you want to delete this food category?';
+    showToast(confirmMsg, 0, {
+        confirm: true,
+        type: 'warning',
+        title: 'Confirm Delete',
+        yesText: 'Delete',
+        noText: 'Cancel',
+        onYes: function () {
+            $.ajax({
+                url: '/Admin/FoodMenuCategories/delete/' + id,
+                type: 'POST',
+                dataType: 'json',
+                success: function (result) {
+                    if (result && result.success) {
+                        showToast('Food category deleted successfully.', 3000, { type: 'success', title: 'Deleted' });
+                        loadFoodCategories();
+                    } else {
+                        showToast('Unable to delete food category.', 3000, { type: 'error', title: 'Delete failed' });
+                    }
+                },
+                error: function () {
+                    showToast('Delete failed.', 3000, { type: 'error', title: 'Delete failed' });
+                }
+            });
+        }
+    });
+}
+
+function setFoodCategoryActive(id, status) {
+    if (!id) return;
+
+    $.ajax({
+        url: '/Admin/FoodMenuCategories/activeinactive?id=' + id + '&status=' + status,
+        type: 'POST',
+        dataType: 'json',
+        success: function (result) {
+            if (result && result.success) {
+                var msg = status ? 'Food category activated.' : 'Food category marked inactive.';
+                showToast(msg, 3000, { type: 'success', title: 'Updated' });
+                loadFoodCategories();
+            } else {
+                showToast('Unable to update food category status.', 3000, { type: 'error', title: 'Update failed' });
+            }
+        },
+        error: function () {
+            showToast('Request failed.', 3000, { type: 'error', title: 'Request failed' });
+        }
+    });
+}
