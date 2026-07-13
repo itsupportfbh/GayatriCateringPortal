16630e4b974e9e47936262c725dce68ea8b20087
POS2 <rajeshkumar16rko@gmail.com>
2026-07-13 15:47:44 +0530

    Category

--- Models/FoodMenuCategory.cs
+++ Models/FoodMenuCategory.cs
@@ -7,11 +7,11 @@ namespace GayatriCateringPortal.Models
         public string Id { get; set; } = null!;
         public string Code { get; set; } = null!;
         public string Name { get; set; } = null!;
-        public string IsActive { get; set; } = null!;
-        public string IsDeleted { get; set; } = null!;
-        public string? CreatedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
         public string CreatedDate { get; set; } = null!;
-        public string? UpdatedBy { get; set; }
+        public int UpdatedBy { get; set; }
         public string? UpdatedDate { get; set; }
     }
 }
