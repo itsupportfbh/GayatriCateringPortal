7621971b873c56a75a92f7f1b6efa124bebf6a02
POS2 <rajeshkumar16rko@gmail.com>
2026-07-15 14:48:28 +0530

    changes

--- wwwroot/js/admin/logistics.js
+++ wwwroot/js/admin/logistics.js
@@ -20,5 +20,39 @@ $(function () {
             t.append('<tr><td>' + id + '</td><td>' + (r.customer || r.Customer || '') + '</td><td>' + (r.event || r.Event || '') + '</td><td>' + (r.date || r.Date || '') + '</td><td>' + (r.driver || r.Driver || '') + '</td><td>' + (r.vehicle || r.Vehicle || '') + '</td><td><span class="badge badge-dispatch">Dispatch</span></td><td><button class="btn btn-primary btn-xs" onclick="markDelivered(\'' + id + '\')">Mark Delivered</button></td></tr>');
         });
     }
-    window.markDelivered=function(id){showToast('Order '+id+' delivered');};
+    window.markDelivered = function (id) { showToast('Order ' + id + ' delivered'); };
+});
+
+$(function () {
+
+    $("#btnPending").click(function () {
+
+        $("#pendingSection").show();
+        $("#deliveredSection").hide();
+
+        $("#btnPending")
+            .removeClass("btn-outline-warning")
+            .addClass("btn-warning");
+
+        $("#btnDelivered")
+            .removeClass("btn-success")
+            .addClass("btn-outline-success");
+
+    });
+
+    $("#btnDelivered").click(function () {
+
+        $("#pendingSection").hide();
+        $("#deliveredSection").show();
+
+        $("#btnDelivered")
+            .removeClass("btn-outline-success")
+            .addClass("btn-success");
+
+        $("#btnPending")
+            .removeClass("btn-warning")
+            .addClass("btn-outline-warning");
+
+    });
+
 });
