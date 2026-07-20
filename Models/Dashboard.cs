0acd5d4e6d3c37cfcde487638946f3d8fe912ad6
POS2 <rajeshkumar16rko@gmail.com>
2026-07-20 08:50:18 +0530

    changes

---
+++ Models/Dashboard.cs
@@ -0,0 +1,8 @@
+﻿namespace GayatriCateringPortal.Models
+{
+    public class Dashboard
+    {
+        public DashboardSummary Summary { get; set; } = new DashboardSummary();
+        public List<DashboardDetails> RecentOrders { get; set; } = new List<DashboardDetails>();
+    }
+}
--- Repositories/DashboardRepository.cs
+++ Repositories/DashboardRepository.cs
@@ -1,37 +1,69 @@
-using System;
-using System.Data;
-using Microsoft.Data.SqlClient;
 using GayatriCateringPortal.Data;
 using GayatriCateringPortal.Interfaces;
+using GayatriCateringPortal.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
 
 namespace GayatriCateringPortal.Repositories;
 
 public class DashboardRepository : IDashboardRepository
 {
-    public object GetSummary()
+    public Dashboard GetDashboard()
     {
-        IDbConnection? conn = null;
-        IDbCommand? cmd = null;
-        try
+        Dashboard dashboard = new Dashboard();
+
+        using (IDbConnection conn = DataFactory.CreateConnection())
         {
-            conn = DataFactory.CreateConnection();
-            cmd = DataFactory.CreateCommand("SP_GetDashboardSummary", conn);
-            ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
             conn.Open();
-            // For simplicity return a placeholder
-            return new { Message = "Summary not implemented" };
+
+            using (IDbCommand cmd = DataFactory.CreateCommand("GetDashboardDetails", conn))
+            {
+                ((SqlCommand)cmd).CommandType = CommandType.StoredProcedure;
+
+                using (SqlDataReader reader = (SqlDataReader)DataFactory.ExecuteReader(cmd))
+                {
+                    // First Result Set - Summary
+                    if (reader.Read())
+                    {
+                        dashboard.Summary.TotalOrders = Convert.ToInt32(reader["TotalOrders"]);
+                        dashboard.Summary.ActiveOrders = Convert.ToInt32(reader["ActiveOrders"]);
+                        dashboard.Summary.TotalSales = Convert.ToDecimal(reader["TotalSales"]);
+                        dashboard.Summary.Outstanding = Convert.ToDecimal(reader["Outstanding"]);
+                    }
+
+                    // Second Result Set - Recent Orders
+                    if (reader.NextResult())
+                    {
+                        while (reader.Read())
+                        {
+                            dashboard.RecentOrders.Add(new DashboardDetails
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                OrderNumber = reader["OrderNumber"].ToString(),
+                                CustomerName = reader["CustomerName"].ToString(),
+                                MobileNo = reader["MobileNo"].ToString(),
+                                EmailId = reader["EmailId"].ToString(),
+                                PackageName = reader["PackageName"].ToString(),
+                                LocationName = reader["LocationName"].ToString(),
+                                EventDate = Convert.ToDateTime(reader["EventDate"]),
+                                MealPeriodName = reader["MealPeriodName"].ToString(),
+                                Pax = Convert.ToInt32(reader["Pax"]),
+                                OrderStatus = Convert.ToInt32(reader["OrderStatus"]),
+                                OrderStatusName = reader["OrderStatusName"].ToString(),
+                                TotalAmount = Convert.ToDecimal(reader["TotalAmount"]),
+                                PaidAmount = Convert.ToDecimal(reader["PaidAmount"]),
+                                PaymentStatus = Convert.ToInt32(reader["PaymentStatus"]),
+                                PaymentStatusName = reader["PaymentStatusName"].ToString(),
+                                DeliveryAddress = reader["DeliveryAddress"].ToString(),
+                                CreatedDate = Convert.ToDateTime(reader["CreatedDate"])
+                            });
+                        }
+                    }
+                }
+            }
         }
-        catch (SqlException)
-        {
-            throw new Exception("Database error");
-        }
-        catch (Exception ex)
-        {
-            throw new Exception(ex.StackTrace);
-        }
-        finally
-        {
-            if (conn != null && conn.State != ConnectionState.Closed) conn.Close();
-        }
+
+        return dashboard;
     }
 }
